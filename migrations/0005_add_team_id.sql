c9503ddeb0a0665491d39840d77ec660fd072989
gadsdencode <23170032-gadsdencode@users.noreply.replit.com>
2026-05-05 19:30:15 +0000

    Git commit prior to merge

--- migrations/0005_add_team_id.sql
+++
@@ -1,5 +0,0 @@
--- Add team_id column to subscriptions table
-ALTER TABLE subscriptions ADD COLUMN IF NOT EXISTS team_id integer;
-
--- Add index for performance
-CREATE INDEX IF NOT EXISTS idx_subscriptions_team_id ON subscriptions(team_id);
